a9bbeff4f51140a4e2fce4471f938ee7a9e948f9
Robin Lichtenthaeler <robin.lichtenthaeler@web.de>
2018-03-01 19:14:21 +0100

    added Problem class

--- src/model/Problem.java
+++ src/model/Problem.java
@@ -3,9 +3,27 @@ package model;
 public class Problem {
 
     public String name;
+    public int rows;
+    public int columns;
 
-    public Problem(String name) {
+    public int noOfVehicles;
+    public List<Vehicles> vehicles;
+
+    public int noOfRides;
+    public List<Ride> rides;
+
+    public int bonus;
+    public int steps;
+
+    public Problem(String name, int rows, int columns, int noOfVehicles, int noOfRides, int bonus, int steps) {
+	super();
 	this.name = name;
+	this.rows = rows;
+	this.columns = columns;
+	this.noOfVehicles = noOfVehicles;
+	this.noOfRides = noOfRides;
+	this.bonus = bonus;
+	this.steps = steps;
     }
 
 }
