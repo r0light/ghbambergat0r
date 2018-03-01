66d1b7eed8c6f500269ce191f0f38f022c9432a7
Jan H. Boockmann <Jan.Boockmann@gmail.com>
2018-03-01 20:00:41 +0100

    save

---
+++ src/algo/SimpleSolver.java
@@ -0,0 +1,18 @@
+package algo;
+
+import model.Problem;
+
+public class SimpleSolver implements Solver {
+
+    @Override
+    public void compute() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setProblem(Problem problem) {
+	// TODO Auto-generated method stub
+
+    }
+}
