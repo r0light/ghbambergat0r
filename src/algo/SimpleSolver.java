d84efb7711343c2be0a6821a4b5daf8ce78a782b
Robin Lichtenthaeler <robin.lichtenthaeler@web.de>
2018-03-01 20:21:13 +0100

    started SimpleSolver

--- src/algo/SimpleSolver.java
+++ src/algo/SimpleSolver.java
@@ -1,6 +1,11 @@
 package algo;
 
+import java.util.List;
+
+import io.OutputWriter;
 import model.Problem;
+import model.Ride;
+import model.Vehicle;
 
 public class SimpleSolver implements Solver {
 
@@ -10,6 +15,29 @@ public class SimpleSolver implements Solver {
     public void compute() {
 	if (problem != null) {
 
+	    List<Ride> rides = problem.rides;
+
+	    for (int i = 0; i < rides.size(); i++) {
+		int minimum = Integer.MAX_VALUE;
+		Vehicle candidate = null;
+		for (int j = 0; j < problem.vehicles.size(); j++) {
+		    int currentDiff = Math.abs(problem.vehicles.get(j).diffTime(rides.get(i)));
+		    if (currentDiff < minimum) {
+			if (problem.vehicles.get(j).makesSense(rides.get(j))) {
+			    minimum = currentDiff;
+			    candidate = problem.vehicles.get(j);
+			}
+		    }
+
+		}
+
+		if (candidate != null) {
+		    candidate.addRide(rides.get(i));
+		}
+	    }
+
+	    OutputWriter outputWriter = new OutputWriter(problem);
+	    outputWriter.write("output/" + problem.name);
 	}
 
     }
