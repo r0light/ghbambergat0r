58b8c84e35d627f0633f61fdfc47b75df29fa9b7
Robin Lichtenthaeler <robin.lichtenthaeler@web.de>
2018-03-01 18:53:28 +0100

    added inputs

--- src/runner/Main.java
+++ src/runner/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) throws ParserException {
 
-	List<String> inputs = Arrays.asList("example.in", "small.in");
+	List<String> inputs = Arrays.asList("a_example.in", "b_should_be_easy.in");
 
 	ExecutorService executor = Executors.newCachedThreadPool();
 	try {
