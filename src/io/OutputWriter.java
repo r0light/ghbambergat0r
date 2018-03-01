47906006e4f80390d729cb46ae8a5c54908c8d92
Robin Lichtenthaeler <robin.lichtenthaeler@web.de>
2018-03-01 20:38:35 +0100

    implemented OutputWriter

--- src/io/OutputWriter.java
+++ src/io/OutputWriter.java
@@ -3,8 +3,13 @@ package io;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 import model.Problem;
+import model.Vehicle;
 
 public class OutputWriter {
     private Problem problem;
@@ -15,6 +20,17 @@ public class OutputWriter {
 
     private String format() {
 	StringBuffer s = new StringBuffer();
+
+	List<String> outputs = new ArrayList<String>();
+	for (Vehicle vehicle : problem.vehicles) {
+	    StringJoiner joinerA = new StringJoiner(" ", "", "");
+	    List<String> rides = vehicle.rides.stream().map(r -> Integer.toString(r.id)).collect(Collectors.toList());
+	    rides.forEach(joinerA::add);
+	    outputs.add((rides.size() + " ") + joinerA.toString());
+	}
+	StringJoiner joinerB = new StringJoiner(System.lineSeparator(), "", "");
+	outputs.forEach(joinerB::add);
+	s.append(joinerB.toString());
 
 	return s.toString();
     }
